07ebc48ee1d1cdcd5351222ca99e9d46b49272e0
Rich Hildred <rhildred@gmail.com>
2013-11-06 16:02:00 -0500

    added hoverbox example with jQuery plugin

---
+++ DocumentRoot/js/hoverBox.js
@@ -0,0 +1,8 @@
+(function($) {
+	$.fn.hoverBox = function() {
+		this.addClass("hoverbox").find("a").each(function(index, a){
+			jQuery(a).append("<img class=\"preview\" src=\"" + a.href + "\"/>");
+		}); 
+		return this;
+	};
+}(jQuery));
