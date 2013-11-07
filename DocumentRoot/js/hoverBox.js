02a78270b744dbfbbbe82cbe9d0f837c886542b7
Rich Hildred <rhildred@gmail.com>
2013-11-06 22:19:40 -0500

    removed local resources and made the css and js come from rhcloud

--- DocumentRoot/js/hoverBox.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-	$.fn.hoverBox = function() {
-		this.addClass("hoverbox").find("a").each(function(index, a){
-			jQuery(a).append("<img class=\"preview\" src=\"" + a.href + "\"/>");
-		}); 
-		return this;
-	};
-}(jQuery));
